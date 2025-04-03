8b94b597129ac0fc2b383ebc99106ac3dec50daa
Odion <odionfiona@gmail.com>
2025-04-03 10:49:23 +0300

    conditional rendering on forms components : src/App.jsx new pictures to be added:   src/assets/nature6.jpg new components created   :   src/components/Forms.jsx Added margintop :   src/components/Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
+import { Forms } from './components/Forms'
 import { Card } from './components/Card'
 import { Navbar } from './components/Navbar'
+import { useState } from 'react'
 
 
 const photos = [
@@ -11,16 +13,28 @@ const photos = [
   'src/assets/photos/nature4.jpg',
   'src/assets/photos/nature5.jpg',
 ]
-
-
 function App() {
+  const [items, setItems] = useState(photos)
+  const [isCollapsed, collapse] = useState(false)
+
+const AddToGallary =() => {
+  setItems(['/src/assets/nature6.jpg', ...items])
+}
+
+const toggle = () => {
+  collapse(!isCollapsed)
+}
+
   return (
     <div className='container'> 
-      <div class='mt-2 text-center'>
-            <Navbar/>
+        <Navbar/>
+      <div class='mt-3 text-center'>
+            {/* <button className='btn btn-primary mx-3' onClick={AddToGallary}>Add</button> */}
+            <button className='btn btn-success float-end' onClick={toggle}>{isCollapsed ? 'Close' : '+Add'}</button>
+            <Forms isVisible={isCollapsed}/>
             <h1>Gallary</h1>
           <div className='row mt-5'>
-           {photos.map((photo) => 
+           {items.map((photo) => 
            <Card src={photo}/> ) }
           </div>
        </div>
